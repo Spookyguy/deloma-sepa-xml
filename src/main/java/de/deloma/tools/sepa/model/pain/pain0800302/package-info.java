7203c04141233d0c804bc4223e8798cc2cd8c6ec
FrankMurmann <frank.murmann@deutschebahn.com>
2025-12-09 06:29:02 +0100

    Removed old generated classes from src/main/java

--- src/main/java/de/deloma/tools/sepa/model/pain/pain0800302/package-info.java
+++
@@ -1,2 +0,0 @@
-@jakarta.xml.bind.annotation.XmlSchema(namespace = "urn:iso:std:iso:20022:tech:xsd:pain.008.003.02", elementFormDefault = jakarta.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package de.deloma.tools.sepa.model.pain.pain0800302;
